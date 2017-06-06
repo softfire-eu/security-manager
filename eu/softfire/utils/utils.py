3b6449cd0f38185945c629890562a211b23e0f31
d.carmignani <d.carmignani@reply.it>
2017-06-06 15:21:48 +0200

    Start implementation of deployment of the firewall on Open Baton

--- eu/softfire/utils/utils.py
+++ eu/softfire/utils/utils.py
@@ -1,8 +1,45 @@
 import logging
 import logging.config
+import random, string, json
+from org.openbaton.cli.agents.agents import OpenBatonAgentFactory
+from sdk.softfire.utils import *
 
 config_path = '/etc/softfire/security-manager/security-manager.ini'
 
 def get_logger(config_path):
     logging.config.fileConfig(config_path)
     return logging.getLogger("security-manager")
+
+def random_string(size):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+
+def deploy_package(path, project_id) :
+    ob_conf = get_config_parser(config_path)["open-baton"]
+
+    '''Open Baton Login'''
+    agent = OpenBatonAgentFactory(nfvo_ip=ob_conf["ip"], nfvo_port=int(ob_conf["port"]), https=(ob_conf["https"] == "True"), version=int(ob_conf["version"]), username=ob_conf["username"], password=ob_conf["password"], project_id=project_id)
+
+    '''Upload the VNFP'''
+    vnfp_agent = agent.get_vnf_package_agent(project_id=project_id)
+    vnfp = vnfp_agent.create(path)
+
+    '''Create and upload the NSD'''
+    #TODO fix
+    nsd_file_path = "etc/resources/nsd-fw.json"
+
+    nsd_agent = agent.get_ns_descriptor_agent(project_id)
+    nsd = {}
+    with open(nsd_file_path, "r") as fd:
+        nsd = json.load(fd)
+    nsd["vnfd"] = [{"id" : vnfp["id"]}]
+    nsd = nsd_agent.create(json.dumps(nsd))
+
+    '''Deploy of the NSR'''
+    nsr_agent = agent.get_ns_records_agent(project_id=project_id)
+    nsr = nsr_agent.create(entity=nsd["id"])
+
+    nsr_details = nsr_agent.find(nsr["id"])
+
+    '''Assuming 1 VNFR, 1 VDU, 1 VNC_Instance, 1 Floating IP'''
+    floating_ip = nsr_details["vnfr"][0]["vdu"][0]["vnfc_instance"][0]["floatingIps"][0]
+    return floating_ip
--- read_template.py
+++ read_template.py
@@ -1,14 +1,46 @@
 from toscaparser.tosca_template import ToscaTemplate as ToscaTemplate
-import yaml
+import yaml, json
 from org.openbaton.cli.agents.agents import OpenBatonAgentFactory
 from sdk.softfire.utils import *
 
 
+def openbaton_login(project_id = "761d8b56-b21a-4db2-b4d2-16b05a01bc7e") :
+    conf = get_config_parser("/etc/softfire/security-manager/security-manager.ini")
+    ob_conf = conf["open-baton"]
+    return OpenBatonAgentFactory(nfvo_ip=ob_conf["ip"], nfvo_port=int(ob_conf["port"]), https=(ob_conf["https"] == "True"), version=int(ob_conf["version"]), username=ob_conf["username"], password=ob_conf["password"], project_id=project_id)
+
+def upload_openbaton_package(path, agent, project_id= "761d8b56-b21a-4db2-b4d2-16b05a01bc7e"):
+    agent = openbaton_login()
+    vnfp_agent = agent.get_vnf_package_agent(project_id=project_id)
+    resp = vnfp_agent.create(path)
+    return resp["id"]
+
+def create_openbaton_nsd(vnfd_id, agent, project_id= "761d8b56-b21a-4db2-b4d2-16b05a01bc7e"):
+    agent = openbaton_login()
+    nsd_agent = agent.get_ns_descriptor_agent(project_id)
+    nsd_file_path = "etc/resources/nsd-fw.json"
+    nsd = {}
+    with open(nsd_file_path, "r") as fd:
+        nsd = json.load(fd)
+    nsd["vnfd"] = [{"id" : vnfd_id}]
+    print(nsd)
+    print(json.dumps(nsd))
+    resp = nsd_agent.create(json.dumps(nsd))
+    return resp
+
+
 def deploy_openbaton_nsr(project_id="761d8b56-b21a-4db2-b4d2-16b05a01bc7e", nsd_id="50361f5e-ff36-4874-b8c1-d3552b8beb80", testbed=None) :
+
+    '''
+    {"keys":[],"configurations":{},"vduVimInstances":{"firewall":["vim-instance-fokus-y2"]}}
+    '''
     #TODO from conf
+    '''
     conf = get_config_parser("etc/security-manager.ini")
     ob_conf = conf["open-baton"]
     agent = OpenBatonAgentFactory(nfvo_ip=ob_conf["ip"], nfvo_port=int(ob_conf["port"]), https=(ob_conf["https"] == "True"), version=int(ob_conf["version"]), username=ob_conf["username"], password=ob_conf["password"], project_id=project_id)
+    '''
+    agent = openbaton_login()
     nsr_ag = agent.get_ns_records_agent(project_id=project_id)
     nsr_ag.create(entity=nsd_id)
 
@@ -39,42 +71,38 @@ def deploy_firewall_resource(node_properties) :
     return None
 
 
-print(get_config(section="messaging", key="bind_port", config_file_path="etc/security-manager.ini"))
-c = get_config_parser("etc/security-manager.ini")
-print(c["open-baton"]["ip"])
-deploy_openbaton_nsr()
 
+if __name__ == "__main__":
+    '''
+    Parse TOSCA template looking for nodes SecurityResource, and create a list
+    '''
+    security_nodes = []
+    #TODO testing from file. To get from experiment manager
+    with open("tosca-ex.yaml", 'r') as stream:
+        try:
+            # Probabilmente posso/devo scegliere un altro modo
+            tpl = ToscaTemplate(yaml_dict_tpl = yaml.load(stream))
+        except Exception as e : #TODO
+            print(e)
+        for node in tpl.nodetemplates:
+            #logger.debug("Found node: %s of type %s with properties: %s" % (
+            #            node.name, node.type, list(node.get_properties().keys())))
+            print("Found node: %s of type %s with properties: %s" % (node.name, node.type, list(node.get_properties().keys())))
+            if node.type == "SecurityResource" :
+                security_nodes.append(node)
 
-'''
-Parse TOSCA template looking for nodes SecurityResource, and create a list
-'''
-security_nodes = []
-#TODO testing from file. To get from experiment manager
-with open("tosca-ex.yaml", 'r') as stream:
-    try:
-        # Probabilmente posso/devo scegliere un altro modo
-        tpl = ToscaTemplate(yaml_dict_tpl = yaml.load(stream))
-    except Exception as e : #TODO
-        print(e)
-    for node in tpl.nodetemplates:
-        #logger.debug("Found node: %s of type %s with properties: %s" % (
-        #            node.name, node.type, list(node.get_properties().keys())))
-        print("Found node: %s of type %s with properties: %s" % (node.name, node.type, list(node.get_properties().keys())))
-        if node.type == "SecurityResource" :
-            security_nodes.append(node)
+    #TODO Check status of resources that the security-manager thinks to be active
 
-#TODO Check status of resources that the security-manager thinks to be active
+    for node in security_nodes :
+        node_properties = node.get_properties()
 
-for node in security_nodes :
-    node_properties = node.get_properties()
-
-    if node_properties["resource_id"].value == "firewall" :
-        ''' The requested resource is a firewall '''
-        deploy_firewall_resource(node_properties)
-    else :
-        ''' ERROR: Unexisting SecurityResource '''
-        #TODO
-        pass
+        if node_properties["resource_id"].value == "firewall" :
+            ''' The requested resource is a firewall '''
+            deploy_firewall_resource(node_properties)
+        else :
+            ''' ERROR: Unexisting SecurityResource '''
+            #TODO
+            pass
 
 
 
