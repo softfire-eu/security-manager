9af6b2a4c150663e43aff9e4986a5ff55f0030fe
d.carmignani <d.carmignani@reply.it>
2017-06-28 11:32:38 +0200

    Removed references to view. Solved crash when lek is not available

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="security-manager",
-    version="0.1",
+    version="0.1.1",
     author="SoftFIRE",
     author_email="softfire@softfire.eu",
     description="The SoftFIRE Security Manager",
