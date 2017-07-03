3eff36993d26666fc634c8fb0b96b6ceba1a7114
d.carmignani <d.carmignani@reply.it>
2017-07-03 16:00:32 +0200

    Updated firewall

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="security-manager",
-    version="0.1.4",
+    version="0.1.5",
     author="SoftFIRE",
     author_email="softfire@softfire.eu",
     description="The SoftFIRE Security Manager",
